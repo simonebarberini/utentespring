214e0511912b3bfc9772fa0815e7e3d23b8d990d
simonebarberini <147926684+simonebarberini@users.noreply.github.com>
2024-06-05 17:35:58 +0200

    first commit

---
+++ src/test/java/com/example/utentespring/UtentespringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.utentespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtentespringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
