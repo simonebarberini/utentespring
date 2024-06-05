b782b789e4dec978609beca350cf58c0ab11e7fe
simonebarberini <147926684+simonebarberini@users.noreply.github.com>
2024-06-05 19:06:20 +0200

    get e post

--- src/main/java/com/example/utentespring/controller/UtenteController.java
+++ src/main/java/com/example/utentespring/controller/UtenteController.java
@@ -1,25 +1,44 @@
 package com.example.utentespring.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.utentespring.Service.UtenteService;
-
+import com.example.utentespring.model.Utente;
+import com.example.utentespring.repository.UtenteRepository;
 
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @RestController
+@RequestMapping("/utenti")
 public class UtenteController {
     @Autowired
     private UtenteService utenteService;
+    @Autowired
+    private UtenteRepository utenteRepository;
 
     @GetMapping("/utente")
     @ResponseBody
     public String stampaUtente(){
         return utenteService.getUtente();
+    }
+
+    @PostMapping("/create")
+    public Utente createUtente(@RequestBody Utente utente) {
+        utenteRepository.save(utente);
+        return utente;
+    }
+
+    @GetMapping("/getUtenti")
+    public List<Utente> getAllUtenti() {
+        return utenteRepository.findAll();
     }
 
     
--- src/main/java/com/example/utentespring/repository/UtenteRepository.java
+++ src/main/java/com/example/utentespring/repository/UtenteRepository.java
@@ -1,14 +1,27 @@
 package com.example.utentespring.repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
 import com.example.utentespring.model.Utente;
 
 @Repository
 public class UtenteRepository {
+
+    private List<Utente> utenti = new ArrayList<>();
     
     public String getUtente(){
         return new Utente("Mirko", "Mirko123").toString();
+    }
+
+    public void save(Utente utente) {
+        utenti.add(utente);
+    }
+
+    public List<Utente> findAll() {
+        return utenti;
     }
 
 }
