1d30d06eed3be166f21d1d4d3e104a3910ff3621
simonebarberini <147926684+simonebarberini@users.noreply.github.com>
2024-06-06 09:05:34 +0200

    controller

--- src/main/java/com/example/utentespring/controller/UtenteController.java
+++ src/main/java/com/example/utentespring/controller/UtenteController.java
@@ -3,6 +3,7 @@ package com.example.utentespring.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.utentespring.Service.UtenteService;
